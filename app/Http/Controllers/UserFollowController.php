92311bc4dbfd21b60307ced35eccc2793e8c0f67
takemichi.niro <ubuntu@ip-172-31-1-243.ap-northeast-1.compute.internal>
2022-03-18 04:55:51 +0000

    timeline

--- app/Http/Controllers/UserFollowController.php
+++ app/Http/Controllers/UserFollowController.php
@@ -4,40 +4,37 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use App\User; // 追加
-
-class UsersController extends Controller
+class UserFollowController extends Controller
 {
-    public function index()
-    {
-        // ユーザ一覧をidの降順で取得
-        $users = User::orderBy('id', 'desc')->paginate(10);
-
-        // ユーザ一覧ビューでそれを表示
-        return view('users.index', [
-            'users' => $users,
-        ]);
-    }
-    
-    public function show($id)
-    {
-        // idの値でユーザを検索して取得
-        $user = User::findOrFail($id);
-        
-        // 関係するモデルの件数をロード
-        $user->loadRelationshipCounts();
-
-        // ユーザの投稿一覧を作成日時の降順で取得
-        $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
-
-        // ユーザ詳細ビューでそれを表示
-        return view('users.show', [
-            'user' => $user,
-            'microposts' => $microposts,
-        ]);
-    }
-    
      /**
+     * ユーザをフォローするアクション。
+     *
+     * @param  $id  相手ユーザのid
+     * @return \Illuminate\Http\Response
+     */
+    public function store($id)
+    {
+        // 認証済みユーザ（閲覧者）が、 idのユーザをフォローする
+        \Auth::user()->follow($id);
+        // 前のURLへリダイレクトさせる
+        return back();
+    }
+
+    /**
+     * ユーザをアンフォローするアクション。
+     *
+     * @param  $id  相手ユーザのid
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        // 認証済みユーザ（閲覧者）が、 idのユーザをアンフォローする
+        \Auth::user()->unfollow($id);
+        // 前のURLへリダイレクトさせる
+        return back();
+    }
+    
+        /**
      * ユーザのフォロー一覧ページを表示するアクション。
      *
      * @param  $id  ユーザのid
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,18 @@
 
 @section('content')
    @if (Auth::check())
-        {{ Auth::user()->name }}
+        <div class="row">
+            <aside class="col-sm-4">
+                {{-- ユーザ情報 --}}
+                @include('users.card')
+            </aside>
+            <div class="col-sm-8">
+                {{-- 投稿フォーム --}}
+                @include('microposts.form')
+                {{-- 投稿一覧 --}}
+                @include('microposts.microposts')
+            </div>
+        </div>
     @else
         <div class="center jumbotron">
             <div class="text-center">
